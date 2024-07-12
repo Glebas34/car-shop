ddfd9a34fa3830bca213feeeee996dc8c3be78e9
Gleb Kuimov <kuimov.gleb30122002@gmail.com>
2024-07-12 23:30:56 +0300

    Поменял API gateway

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -1,26 +1,9 @@
-using System.Net.Security;
-using System.Security.Cryptography.X509Certificates;
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var cert = new X509Certificate2("certificates/aspnetapp.pfx", "1234");
-
 builder.Services.AddReverseProxy()
-    .ConfigureHttpClient((context, handler) =>
-        {
-            handler.SslOptions = new SslClientAuthenticationOptions
-            {
-                ClientCertificates = new X509CertificateCollection
-                {
-                    cert
-                }
-            };
-        
-
-        })
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 builder.Services.AddCors();
